571f2d7ecd718525d7772106b5b35765095d8da3
Your Name <mohiniawate>
2024-06-21 16:02:42 +0530

    change in settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
 '''Configuration for production mode'''
 from .base import *  # pylint: disable=wildcard-import
 from .env import abs_path, env_setting
+from decouple import config
+
+SECRET_KEY = config('DJANGO_SECRET_KEY', default=None)
+
+if SECRET_KEY is None:
+    raise ValueError("The SECRET_KEY setting must not be empty")
+
+print(f"SECRET_KEY is: {SECRET_KEY}")
 
 # Set ALLOWED_HOSTS to the specific domains your application will serve
 ALLOWED_HOSTS = env_setting('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
