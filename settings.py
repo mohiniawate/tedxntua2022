bd3c129ffafde16e9eb66da0f597526f2958005f
Your Name <mohiniawate>
2024-06-21 17:45:21 +0530

    settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from .base import *  # pylint: disable=wildcard-import
 from .env import abs_path, env_setting
 import os
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = "iit*qlpw9b71els9)14mhsk@i3t1_u6x!hv2c5!-x7^r1&^n6p"
 
 # Set ALLOWED_HOSTS to the specific domains your application will serve
 ALLOWED_HOSTS = env_setting('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
